829cb8075f7df08d12604bf7288bb6d6083ea232
kjk080kjk0@mail.ru <kjk080kjk0@mail.ru>
2023-11-02 22:58:28 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System.IO;
+
+namespace TaskOne
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string path = "E:\\Games\\7 Days To Die";
+            ClearDirectory CDirectory = new(30);
+            CDirectory.Checked(path);
+        }
+    }
+    class ClearDirectory
+    {
+        TimeSpan Time;
+
+        public ClearDirectory(byte time) 
+        {
+            Time = TimeSpan.FromMinutes(time);
+        }
+
+        public void Checked(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            try
+            {
+                if (dir.Exists)
+                {
+                    var files = dir.GetFiles();
+                    foreach(var file in files)
+                    {
+                        if ((DateTime.Now - file.LastWriteTime) >= Time)
+                        {
+                            Remove($"Файл на удаление {file.Name}");
+                            continue;
+                        }
+                    }
+                    var directories = dir.GetDirectories();
+                    foreach(var directory in directories)
+                    {
+                        Checked(directory.FullName);
+                        if ((DateTime.Now - directory.LastWriteTime) >= Time)
+                        {
+                            Remove($"Директория на удаление {directory.Name}");
+                        }
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Remove(string path)
+        {
+            Console.WriteLine(path);
+        }
+
+    }
+}
